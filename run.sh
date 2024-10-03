0405ebc403c5fc5e4b70129546e0fd8cbcb4eafe
Pengxiang Huang <huangpengxiang70@gmail.com>
2024-10-02 21:30:48 -0500

    add the cmake and build files

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+export CC=clang
+export CXX=clang++
+
+# Check the cmake binary
+CMAKE="cmake" ;
+
+# Clean
+rm -rf build/ ; 
+
+# Build
+mkdir build ; 
+cd build ; 
+${CMAKE} -DCMAKE_INSTALL_PREFIX="~/CAT" -DCMAKE_BUILD_TYPE=Debug ../ ; 
+make 
---
+++ src/CFIEnforce.cpp
@@ -0,0 +1,43 @@
+// #include "llvm/Pass.h"
+// #include "llvm/IR/Function.h"
+// #include "llvm/Support/raw_ostream.h"
+// #include "llvm/IR/LegacyPassManager.h"
+// #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+//
+// using namespace llvm;
+//
+// namespace {
+//   struct CAT : public FunctionPass {
+//     static char ID; 
+//
+//     CAT() : FunctionPass(ID) {}
+//
+//     bool doInitialization (Module &M) override {
+//       errs() << "Hello LLVM World at \"doInitialization\"\n" ;
+//       return false;
+//     }
+//
+//     bool runOnFunction (Function &F) override {
+//       errs() << "Hello LLVM World at \"runOnFunction\"\n" ;
+//       return false;
+//     }
+//
+//     void getAnalysisUsage(AnalysisUsage &AU) const override {
+//       errs() << "Hello LLVM World at \"getAnalysisUsage\"\n" ;
+//       AU.setPreservesAll();
+//     }
+//   };
+// }
+//
+// // Next there is code to register your pass to "opt"
+// char CFIEnforce::ID = 0;
+// static RegisterPass<CFIEnforce> X("CFIEnforce", "CFIEnforcement");
+//
+// // Next there is code to register your pass to "clang"
+// static CFIEnforce * _PassMaker = NULL;
+// static RegisterStandardPasses _RegPass1(PassManagerBuilder::EP_OptimizerLast,
+//     [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+//         if(!_PassMaker){ PM.add(_PassMaker = new CFIEnforce());}}); // ** for -Ox
+// static RegisterStandardPasses _RegPass2(PassManagerBuilder::EP_EnabledOnOptLevel0,
+//     [](const PassManagerBuilder&, legacy::PassManagerBase& PM) {
+//         if(!_PassMaker){ PM.add(_PassMaker = new CFIEnforce()); }}); // ** for -O0
