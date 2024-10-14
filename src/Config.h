9e941b9f9c17c2a409f827266b84e57f36a37ee8
Pengxiang Huang <huangpengxiang70@gmail.com>
2024-10-14 11:03:19 -0500

    change the structure to executable from a pass since it can take multiple bc files

---
+++ src/Config.h
@@ -0,0 +1,25 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+#include "Utils.h"
+
+using namespace std;
+using namespace llvm;
+
+//
+// Configurations
+//
+
+#define DEBUG
+
+#define SOUND_MODE 1
+#define MAX_TYPE_LAYER 10
+
+#define MAP_CALLER_TO_CALLEE 1
+#define UNROLL_LOOP_ONCE 1
+#define MAP_DECLARATION_FUNCTION
+#define PRINT_ICALL_TARGET
+#define SOURCE_CODE_PATH "/home/kjlu/projects/kernels/linux-5.1"
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/ToolOutputFile.h"
+#include "llvm/Support/SystemUtils.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/Path.h"
+
+#include "Utils.h"
+
+using namespace llvm;
+
+
+// Command line parameters.
+cl::list<std::string> InputFilenames(
+    cl::Positional, cl::OneOrMore, cl::desc("<input bitcode files>"));
+
+cl::opt<unsigned> VerboseLevel(
+    "verbose-level", cl::desc("Print information at which verbose level"),
+    cl::init(0));
+
+
+int main(int argc, char **argv) {
+
+	// Print a stack trace if we signal out.
+	sys::PrintStackTraceOnErrorSignal(argv[0]);
+	PrettyStackTraceProgram X(argc, argv);
+
+	llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
+
+	cl::ParseCommandLineOptions(argc, argv, "global analysis\n");
+	SMDiagnostic Err;
+
+	OP << "Hello world here \n";
+
+	// Loading modules
+	OP << "Total " << InputFilenames.size() << " file(s)\n";
+
+	for (unsigned i = 0; i < InputFilenames.size(); ++i) {
+
+		LLVMContext *LLVMCtx = new LLVMContext();
+		std::unique_ptr<Module> M = parseIRFile(InputFilenames[i], Err, *LLVMCtx);
+
+		if (M == NULL) {
+			OP << argv[0] << ": error loading file '"
+				<< InputFilenames[i] << "'\n";
+			continue;
+		}
+
+		// Module *Module = M.release();
+		// StringRef MName = StringRef(strdup(InputFilenames[i].data()));
+	}
+	return 0;
+}
